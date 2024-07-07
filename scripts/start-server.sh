6f22658e7e39fdfc475a4f07cd6be423ff2e52c6
yongtaeyu <yongtaeyu@mz.co.kr>
2024-07-07 14:42:55 +0900

    컨테이너기반2 fix

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/test-deploy
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop test-deploy-server || true
+docker rm test-deploy-server || true
+docker pull /test-deploy-ecr:latest
+docker run -d --name test-deploy-server -p 8080:8080 ${{ steps.login-ecr.outputs.registry }}/test-deploy-ecr:latest
 echo "--------------- 서버 배포 끝 -----------------"
