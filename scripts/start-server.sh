c2f3597c740d110c563bda4890c416978516858b
yongtaeyu <yongtaeyu@mz.co.kr>
2024-07-07 14:55:06 +0900

    컨테이너기반2 fix

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop test-deploy-server || true
 docker rm test-deploy-server || true
-docker pull /test-deploy-ecr:latest
-docker run -d --name test-deploy-server -p 8080:8080 ${{ steps.login-ecr.outputs.registry }}/test-deploy-ecr:latest
+docker pull 860822614692.dkr.ecr.ap-northeast-2.amazonaws.com/test-deploy-ecr
+docker run -d --name test-deploy-server -p 8080:8080 860822614692.dkr.ecr.ap-northeast-2.amazonaws.com/test-deploy-ecr:latest
 echo "--------------- 서버 배포 끝 -----------------"
